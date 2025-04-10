3c74b49166510f06c5904be311df168d27b871f2
Diego <diegorandal@gmail.com>
2025-04-10 14:19:52 -0300

    pc sape

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,20 @@ import { PayBlock } from "./components/Pay";
 import { VerifyBlock } from "./components/Verify";
 
 export default function App() {
+  
+  const handleClick = () => {
+    console.log("Respuesta de verify: sape");
+};
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
       <VerifyBlock />
       <PayBlock />
+      <div>
+            <h1>Mi MiniApp 2</h1>
+            <button onClick={handleClick}>Probar MiniKit</button>
+        </div>
     </main>
   );
 }
+
