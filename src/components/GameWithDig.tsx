b7f8554937e4fb31eb6337ed45919c0204101ec9
Diego <diegorandal@gmail.com>
2025-06-17 21:35:58 -0300

    sendTransaction without ORO SC approve

--- src/components/GameWithDig.tsx
+++ src/components/GameWithDig.tsx
@@ -103,15 +103,6 @@ export default function GameWithDig() {
       await MiniKit.commandsAsync.sendTransaction({
         transaction: [
           {
-            address: '0xcd1E32B86953D79a6AC58e813D2EA7a1790cAb63',
-            abi: ORO_ABI,
-            functionName: 'approve',
-            args: [
-              '0xe2B81493d6C26E705bc4193A87673db07810f376',
-              oroAmount
-            ]
-          },
-          {
             address: '0xe2B81493d6C26E705bc4193A87673db07810f376',
             abi: TreasureHuntABI,
             functionName: 'dig',
